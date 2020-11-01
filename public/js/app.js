a67f1073476ceef0ff66ab681c3e4bf7b8c941c9
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-11-01 12:40:53 +0900

    ログインページの実装を行った。

--- public/js/app.js
+++ public/js/app.js
@@ -37330,16 +37330,25 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+// changeイベントは、フォーム要素が変更された時に発火するイベント
 $(document).on("change", "#file_photo", function (e) {
-  var reader;
+  // varで変数readerを定義する
+  var reader; // ファイルの有無を判定
 
   if (e.target.files.length) {
-    reader = new FileReader();
+    // JavaScriptでファイル操作をしたい時はFileReaderのオブジェクトを作成
+    // 4行目で定義したreaderという変数に、FileReaderオブジェクトのインスタンスを代入
+    // これでFileReaderに関するオブジェクトが、readerを用いて使用可能に！
+    reader = new FileReader(); // ファイルの読み込みがうまくいけば、reader.onloadイベントが発生
 
     reader.onload = function (e) {
-      var userThumbnail;
-      userThumbnail = document.getElementById('thumbnail');
-      $("#userImgPreview").addClass("is-active");
+      // varで変数userThumbnail(サムネイル)を定義する
+      var userThumbnail; // プレビューを表示するための要素を取得
+
+      userThumbnail = document.getElementById('thumbnail'); // 取得したファイルを表示させるために、.is - activeクラスを付与
+
+      $("#userImgPreview").addClass("is-active"); // プレビュー画像を表示するためにimgタグのsrc属性に、e.target.resultで取得したファイル名を設定
+
       userThumbnail.setAttribute('src', e.target.result);
     };
 
