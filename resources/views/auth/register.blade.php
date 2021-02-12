11b3f224d083ee4289de586c1c6b82eb5577753a
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2021-02-12 20:51:49 +0900

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.layout')
 
+<script src="{{ asset('js/user.js') }}"></script>
+
 @section('content')
 
 <div class="signupPage">
@@ -105,31 +107,3 @@
   </div>
 </div>
 @endsection
-
-<!-- user.js -->
-<script>
-// changeイベントは、フォーム要素が変更された時に発火するイベント
-$(document).on("change", "#file_photo", function (e) {
-  // varで変数readerを定義する
-  var reader;
-  // ファイルの有無を判定
-  if (e.target.files.length) {
-    // JavaScriptでファイル操作をしたい時はFileReaderのオブジェクトを作成
-    // 4行目で定義したreaderという変数に、FileReaderオブジェクトのインスタンスを代入
-    // これでFileReaderに関するオブジェクトが、readerを用いて使用可能に！
-    reader = new FileReader;
-    // ファイルの読み込みがうまくいけば、reader.onloadイベントが発生
-    reader.onload = function (e) {
-      // varで変数userThumbnail(サムネイル)を定義する
-      var userThumbnail;
-      // プレビューを表示するための要素を取得
-      userThumbnail = document.getElementById('thumbnail');
-      // 取得したファイルを表示させるために、.is - activeクラスを付与
-      $("#userImgPreview").addClass("is-active");
-      // プレビュー画像を表示するためにimgタグのsrc属性に、e.target.resultで取得したファイル名を設定
-      userThumbnail.setAttribute('src', e.target.result);
-    };
-    return reader.readAsDataURL(e.target.files[0]);
-  }
-});
-</script>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
